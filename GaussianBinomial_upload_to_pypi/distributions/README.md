a2dfe097ec22a510d23be0e1ce1ee0473531ac8c
Chauhanshi <50184931+Chauhanshi@users.noreply.github.com>
2020-06-21 19:09:53 -0400

    added all the files

---
+++ GaussianBinomial_upload_to_pypi/distributions/README.md
@@ -0,0 +1,9 @@
+# GaussianBinomial-distribution Documentation
+
+Summary of the package
+
+# Files
+
+Explanation of the files in the package
+
+# Installation 
---
+++ GaussianBinomial_upload_to_pypi/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GaussianBinomial-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['GaussianBinomial-distributions'],
+      author = 'Shivam Chauhan',
+      author_email = 'shivam.chauhan21@gmail.com',
+      zip_safe=False)
