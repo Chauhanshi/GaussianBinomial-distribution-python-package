3c9f032c4a8204cb3ab9f777c3c1ae04945fd267
Chauhanshi <50184931+Chauhanshi@users.noreply.github.com>
2020-06-22 11:34:49 -0400

    files after uploading to pypi (.tar.gz)

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
-from setuptools import setup
+import setuptools
 
-setup(name='GaussianBinomial-distributions',
-      version='0.1',
-      description='Gaussian distributions',
-      packages=['GaussianBinomial-distributions'],
-      author = 'Shivam Chauhan',
-      author_email = 'shivam.chauhan21@gmail.com',
-      zip_safe=False)
+
+setuptools.setup(
+    name="GandB-distributions-chauhan.shi",
+    version="0.0.1",
+    author="Shivam Chauhan",
+    author_email="shivam.chauhan21@gmail.com",
+    description="Guassian and Binomial distribution package",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Chauhanshi/GaussianBinomial-distribution-python-package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
